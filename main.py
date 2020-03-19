0561424041ddd977a09175c0e57cf2c6a5d21f5e
kernelMaster47 <l.bonassi005@studenti.unibs.it>
2020-03-19 16:06:03 +0100

    rwGgergradfdaad

---
+++ main.py
@@ -0,0 +1,33 @@
+import neuralNet
+import numpy as np
+import random
+
+
+def getPlan(nrow, ncol, ngoal):
+    x = np.zeros((nrow, ncol), dtype="float")
+    for i in range(nrow):
+        index = random.randint(0, ncol-1)
+        x[i][index] = 1
+    y = np.zeros(ngoal)
+    index = random.randint(0, ngoal - 1)
+    y[index] = 1
+    return x, y
+
+
+def create_plans(nrow, ncol, ngoal, nplans):
+    plans_X = np.empty((nplans, nrow, ncol))
+    plans_Y = np.empty((nplans, ngoal))
+    for i in range(nplans):
+        plan = getPlan(nrow, ncol, ngoal)
+        plans_X[i] = plan[0]
+        plans_Y[i] = plan[1]
+    return plans_X, plans_Y
+
+
+if __name__ == '__main__':
+    X_train, Y_train = create_plans(20, 100, 5, 10)
+
+    model = neuralNet.get_net2(20, 100, 5)
+    model.fit(X_train, Y_train, batch_size=10, epochs=60, verbose=2)
+    print("ciao")
+
---
+++ neuralNet.py
@@ -0,0 +1,29 @@
+import numpy as np
+from keras.models import Model
+from keras.layers import Input
+from keras.layers import Dense, LSTM
+from keras.models import Sequential
+from AttentionMechanism import AttentionL
+import keras
+
+
+def get_net(len_sequance, dim_input, dim_output):
+    input = Input(shape=(len_sequance, dim_input))
+    l = LSTM(64, activation="linear")(input)
+    o = Dense(dim_output, activation="softmax")(l)
+    model = Model(input=input, outputs=o)
+    #adam = keras.optimizers.Adam(lr=0.1, beta_1=0.9, beta_2=0.999, amsgrad=False)
+    sgd = keras.optimizers.SGD(lr=0.1)
+    model.compile(optimizer=sgd, loss='categorical_crossentropy', metrics=['accuracy'])
+    return model
+
+
+def get_net2(len_sequance, dim_input, dim_output):
+    model = Sequential()
+    model.add(LSTM(64, activation="linear", return_sequences=True))
+    model.add(AttentionL(20))
+    model.add(Dense(dim_output, activation="softmax"))
+    sgd = keras.optimizers.SGD(lr=0.1)
+    model.compile(optimizer=sgd, loss='categorical_crossentropy', metrics=['accuracy'])
+    return model
+
